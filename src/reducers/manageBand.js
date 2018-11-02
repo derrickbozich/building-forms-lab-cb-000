fe1d999cc51ae8c57df3c5f1eb37676a49a851a9
derrick b <dbozich1@gmail.com>
2018-11-02 02:37:19 +0000

    automatically backed up by learn

--- src/reducers/manageBand.js
+++ src/reducers/manageBand.js
@@ -1,7 +1,6 @@
 export default function manageBand(state = {bands: [],}, action) {
   switch (action.type) {
     case 'ADD_BAND':
-
       return {...state, bands: [...state.bands, action.payload]}
     default:
       return state;
