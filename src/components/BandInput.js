b131bd2052291f87697225af09e19db64a60a8ec
derrick b <dbozich1@gmail.com>
2018-11-02 02:27:23 +0000

    automatically backed up by learn

--- src/components/BandInput.js
+++ src/components/BandInput.js
@@ -2,9 +2,22 @@
 import React, { Component } from 'react'
 
 class BandInput extends Component {
+
+  state = {
+    name: ''
+  }
+
+  handleChange = e => {
+    this.setState({
+      name: e.tarvet.value
+    })
+  }
+
+
   render() {
     return(
       <div>
+        <input type="text" onChange={this.handleChange} value={this.state.name} />
         Band Input
       </div>
     )
